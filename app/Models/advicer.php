cc6d85b42b406d5be3844d4471365c1e57b50f08
Godo25 <gudo.alfred@gmail.com>
2023-12-27 01:33:14 -0800

    database

---
+++ app/Models/advicer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class advicer extends Model
+{
+    use HasFactory;
+}
